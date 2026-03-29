c4c83943fb5c0d6bc13f82006095fa0b9dd2889f
Amadou <116417004+Amadou-D@users.noreply.github.com>
2026-03-29 13:40:46 -0600

    Update page.tsx

--- my-app/app/projects/page.tsx
+++ my-app/app/projects/page.tsx
@@ -8,7 +8,6 @@ import { useRouter } from 'next/navigation';
 const projects = [
   { title: '5 Oceans Immigration -  A Full stack Web Application I made for a Client', videoSrc: '/5oceans.mp4', url: 'https://5-oceans-immigration-amadou-diallos-projects.vercel.app/homepage' },
   { title: 'Khazad Tech - an example e-commerce Site I made', videoSrc: '/tech.mp4', url: 'https://webdev2-final-project-sooty.vercel.app' },
-  { title: 'Music Visualizer - An Audio to Visual Tool', videoSrc: '/music_visualizer.mp4', url: 'https://music-visualizeer.vercel.app' },
   { title: 'Pokehub - Pokémon TCG Dex', videoSrc: '/pokemon.mp4', url: 'https://amadou-d.github.io/Pokehub/' },
 ];
 
